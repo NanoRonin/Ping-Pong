2febd9317a775e4bd13a0679564efa0f0bc015ea
NanoRonin <rohinthespiderman@gmail.com>
2026-06-07 10:48:01 -0500

    Finished Player
    Finished the player movement and collisions with the walls

---
+++ Scenes/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+
+func _physics_process(_delta: float):
+	velocity.y = 0
+	
+	if Input.is_action_pressed("playerUp"):
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("playerDown"):
+		velocity.y = SPEED
+	
+	move_and_slide()
+	
+	var paddleHeight = $Collision.shape.size.y
+	var viewportHeight = get_viewport_rect().size.y
+	
+	global_position.y = clamp(global_position.y, 0, viewportHeight - paddleHeight)
